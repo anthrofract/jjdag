4582fe8ab9e72ac89a261e43e21005068da8cd3a
Anthrofract <anthrofract@fastmail.com>
2026-02-11 01:03:13 -0600

    Tweak main.rs

--- src/main.rs
+++ src/main.rs
@@ -18,25 +18,27 @@ use shell_out::JjCommand;
 use terminal::Term;
 
 fn main() {
-    if let Err(err) = _main() {
+    let result = run();
+    if let Err(err) = result {
+        // Avoids a redundant message "Error: Error:"
         eprintln!("{err}");
         std::process::exit(1);
     }
 }
 
-fn _main() -> Result<()> {
+fn run() -> Result<()> {
     let args = Args::parse();
     let repository = JjCommand::ensure_valid_repo(&args.repository)?;
     let model = Model::new(repository, args.revisions)?;
 
     let terminal = terminal::init_terminal()?;
-    let result = main_loop(model, terminal);
+    let result = tui_loop(model, terminal);
     terminal::relinquish_terminal()?;
 
     result
 }
 
-fn main_loop(mut model: Model, terminal: Term) -> Result<()> {
+fn tui_loop(mut model: Model, terminal: Term) -> Result<()> {
     while model.state != State::Quit {
         terminal.borrow_mut().draw(|f| view(&mut model, f))?;
         update(terminal.clone(), &mut model)?;
